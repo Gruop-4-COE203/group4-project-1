22f7b78d857b4fc28611f621956fd8333363b7af
ipekorkmaz <2309011084@stu.istinye.edu.tr>
2025-10-31 22:49:19 +0300

    ipek

--- py_prj.py
+++ py_prj.py
@@ -17,6 +17,7 @@ if "TRY" not in rates_dict:
     rates_dict["TRY"] = 1.0
 
 # How many exchange rates loaded
+
 print(f"{len(exchange_rates)} types of exchange you can exchange:")
 for line in exchange_rates:
     currency, rate = line.split("=")
@@ -26,6 +27,16 @@ for line in exchange_rates:
 
 # Getting user inputs for conversion
 user_input_convert_from = input("\nEnter the currency unit that you want from convert(e.g. USD, TRY, EUR, JPY, SAR, GBP):")
+
+print(f"{len(exchange_rates)} types of exchange you can exchange:\n")
+for line in exchange_rates:
+    currency, rate = line.split("=")
+    print(f"{currency} : {rate}")
+
+
+# Getting user inputs for conversion
+user_input_convert_from = input("Enter the currency unit that you want from convert(e.g. USD, TRY, EUR, JPY, SAR, GBP):")
+
 user_input_convert_to = input("Enter the currency unit that you want to convert from (e.g. USD, TRY, EUR, JPY, SAR, GBP):")
 
 
@@ -36,7 +47,11 @@ convert_from = user_input_convert_from.upper()
 
 # Validating user input currency and getting amount to convert
 if convert_from in ["USD", "TRY", "EUR", "JPY", "SAR", "GBP"] and convert_to in ["USD", "TRY", "EUR", "JPY", "SAR", "GBP"]:
+
     user_input_amount = float(input("Enter the amount of money you want to convert:"))
+    user_input_amounts = input("Enter one or more amounts separated by commas (e.g. 100, 250, 500): ").split(",")
+    amounts = [float(x.strip()) for x in user_input_amounts]
+
 else:
     print("Invalid currency.Please try again!")
     exit()
@@ -60,6 +75,12 @@ def calculate_exchange(from_currency, to_currency, amount):
     else:
         return "Currency not found in exchange_rate.txt file!"
 # Calculate using user input
+
 result = calculate_exchange(convert_from, convert_to, user_input_amount)
 print(result)
+
 #zehra
+
+for amount in amounts:
+    result = calculate_exchange(convert_from, convert_to, amount)
+    print(result)
